529b362ff20b4c0dcd944535022a1712647abc3b
wraith4081 <54374743+wraith4081@users.noreply.github.com>
2023-06-29 00:08:28 +0000

    Fixed: node-fetch's version error

--- dist/types/ErrorResponse.d.ts
+++
@@ -1,4 +0,0 @@
-export default interface ErrorResponse {
-    HttpCode: number;
-    Message: string;
-}
--- dist/utils/StorageEndpoints.d.ts
+++
@@ -1,8 +0,0 @@
-declare enum StorageEndpoints {
-    'Falkenstein' = "storage.bunnycdn.com",
-    'NY' = "ny.storage.bunnycdn.com",
-    'LA' = "la.storage.bunnycdn.com",
-    'Singapore' = "sg.storage.bunnycdn.com",
-    'Sydney' = "syd.storage.bunnycdn.com"
-}
-export default StorageEndpoints;
